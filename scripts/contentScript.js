1c40f17eae18485da50fd9ba441b7eb1d7659cf3
dval <dylan.valentine@gmail.com>
2023-04-17 18:06:10 -0400

    removed unused functions

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -5,32 +5,6 @@
  * @version 1.0.0
  */
 
-// This function does not work due to security something or other.
-// function pasteTextIntoDiscord(text) {
-//   const chatInput = document.querySelector('div[data-slate-editor="true"][contenteditable="true"]');
-//   if (chatInput) {
-//     const pasteEvent = new ClipboardEvent('paste', {
-//       dataType: 'text/plain',
-//       data: text
-//     });
-//     chatInput.dispatchEvent(pasteEvent);
-//   }
-// }
-
-// Get clipboard contents
-function readClipboard() {
-  navigator.clipboard.readText()
-  .then((text) => {
-    console.log("pasteText: " + text);
-    // auto-paste the text into the Discord chat input
-    // pasteTextIntoDiscord(text);
-    window.removeEventListener('focus', readClipboard);
-  })
-  .catch((err) => {
-    console.error('Failed to read clipboard contents: ', err.message);
-  });
-}
-
 // listen for messages from the popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'pasteText') {
